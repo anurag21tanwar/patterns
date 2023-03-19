15b113e8683b557429fe15be4cb4d5097b7c2ff1
Anurag Tanwar <anurag21tanwar@gmail.com>
2023-03-19 21:07:07 +0530

    chain of responsibility pattern

--- src/main/java/com/upgrad/patterns/Authentication/Authenticator.java
+++ src/main/java/com/upgrad/patterns/Authentication/Authenticator.java
@@ -7,12 +7,10 @@ import com.upgrad.patterns.Middleware.JwtAuthProcessor;
 import javax.servlet.http.HttpServletRequest;
 
 public class Authenticator {
-	
-//create a public static method GetAuthProcessor of the return type AuthenticationProcessor
-	// create an object of type JwtAuthProcessor
-	// Chain Authentication processors, first JWT processor is to be used first and then basic auth processor
-	// return the object
-	
+
+    public static AuthenticationProcessor GetAuthProcessor(){
+        return new JwtAuthProcessor(new BasicAuthProcessor(null));
+    }
 
     public static AuthenticationProvider GetAuthProvider(HttpServletRequest request)
     {
